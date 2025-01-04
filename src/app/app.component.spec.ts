bc47348473f7070319a9be0e2b8b646c52e25814
atilaalcantara <atila.alcantara@gavresorts.com.br>
2025-01-03 22:52:09 -0300

    first-commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'calculadora-despesas' title`, () => {
+  it(`should have the 'PairProgress' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('calculadora-despesas');
+    expect(app.title).toEqual('PairProgress');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, calculadora-despesas');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, PairProgress'
+    );
   });
 });
