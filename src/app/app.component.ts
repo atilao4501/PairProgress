794f35787e212994c8db1db489804862fd40d40c
atilaalcantara <atila.alcantara@gavresorts.com.br>
2025-01-21 19:02:32 -0300

    feat: login page without logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,13 @@ import { MascotStatusComponent } from "../components/mascot-status/mascot-status
 import { DashboardComponent } from "../pages/dashboard/dashboard.component";
 import { MonthlyContributionGridComponent } from "../components/monthly-contribution-grid/monthly-contribution-grid.component";
 import { CreateGoalModalComponent } from "../components/create-goal-modal/create-goal-modal.component";
+import { LoginCardComponent } from "../components/login-card/login-card.component";
+import { LoginComponent } from "../pages/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, GoalsAccordionComponent, MascotStatusComponent, DashboardComponent, MonthlyContributionGridComponent, CreateGoalModalComponent],
+  imports: [RouterOutlet, HeaderComponent, GoalsAccordionComponent, MascotStatusComponent, DashboardComponent, MonthlyContributionGridComponent, CreateGoalModalComponent, LoginCardComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
